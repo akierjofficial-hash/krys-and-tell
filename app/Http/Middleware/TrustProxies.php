e4d61fcb1032d624c6e0362ccc06c5f243127d6f
kier.dev <akierjofficial@gmail.com>
2025-12-24 02:05:58 +0800

    Fix TrustProxies header constants for Render

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,21 +3,26 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
     /**
-     * Trust all proxies (Render / load balancers).
+     * Trust all proxies (Render / nginx / etc.)
      *
-     * @var array<int, string>|string|null
+     * @var array|string|null
      */
     protected $proxies = '*';
 
     /**
-     * Trust all forwarded headers.
+     * Use Symfony constants (works across Laravel versions)
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_PREFIX;
 }
